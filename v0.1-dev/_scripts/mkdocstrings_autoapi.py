84adbf55db0b91a87bc4b47a7cc816245b434648
Richard Sandoval <rsandovaldev@gmail.com>
2025-02-24 11:09:41 +0100

    Deployed 1f8b6f9 to v0.1-dev with MkDocs 1.6.1 and mike 2.1.3

---
+++ v0.1-dev/_scripts/mkdocstrings_autoapi.py
@@ -0,0 +1,9 @@
+# License: MIT
+# Copyright © 2025 Frequenz Energy-as-a-Service GmbH
+
+"""Generate the code reference pages."""
+
+from frequenz.repo.config.mkdocs import api_pages
+
+api_pages.generate_python_api_pages("py", "python-reference")
+api_pages.generate_protobuf_api_pages()
